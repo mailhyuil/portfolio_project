2d68f3f5f4b41fbd0ec6addff8c3d77a37f2286e
mailhyuil <mailhyuil@gmail.com>
2022-10-10 18:49:57 +0900

    first

---
+++ src/test/java/com/soosan/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soosan.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
